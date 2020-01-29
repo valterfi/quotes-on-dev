50d711054cf111dcc7196f3d155276ec71190cf0
Valterfi <morpheuw@gmail.com>
2020-01-28 20:20:40 -0800

    change the jquery function name

--- themes/quotesondev/js/get-quote.js
+++ themes/quotesondev/js/get-quote.js
@@ -1,14 +1,18 @@
-jQuery(function () {
+(function($) {
 
-    jQuery('#new-quote-button').click(function() {
-        jQuery.ajax({
-            url: qod_vars.rest_url + 'wp/v2/posts/?&filter[posts_per_page]=1&filter[orderby]=rand',
-            context: document.body
-          }).done(function(data) {
-            if (typeof data !== 'undefined' && data.length > 0) {
-                jQuery('.entry-content').html(data[0].content.rendered);
-            }
+    $(function () {
+
+        $('#new-quote-button').click(function() {
+            jQuery.ajax({
+                url: qod_vars.rest_url + 'wp/v2/posts/?&filter[posts_per_page]=1&filter[orderby]=rand',
+                context: document.body
+            }).done(function(data) {
+                if (typeof data !== 'undefined' && data.length > 0) {
+                    $('.entry-content').html(data[0].content.rendered);
+                }
+            });
         });
+
     });
 
-});
+})( jQuery );
