d90e8af748a2abe2a334b45426d9242257a34e48
Valterfi <morpheuw@gmail.com>
2020-02-05 03:05:21 -0800

    installing font awesome and styling 404.php category.php and tag.php

--- themes/quotesondev/searchform.php
+++ themes/quotesondev/searchform.php
@@ -4,7 +4,10 @@
 			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
 		<button class="search-submit">
-			<?php echo esc_html( 'Search' ); ?>
+			<span class="icon-search" aria-hidden="true">
+				<i class="fa fa-search"></i>
+			</span>
+			<span class="screen-reader-text">Search</span>
 		</button>
 	</fieldset>
 </form>
